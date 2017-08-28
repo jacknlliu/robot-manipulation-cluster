32acda7eb9e4dbd86c18dffea5ed12cf6b91746d
Jack Liu <jacknlliu@gmail.com>
2017-08-28 15:32:50 +0800

    fix(learning module): fix jupyter workspace data storage

--- Learning-Module/start-docker.sh
+++ Learning-Module/start-docker.sh
@@ -143,7 +143,7 @@ if [[ -f $CONTAINER_INIT_DIR/start-dapplications.sh ]]; then
          --security-opt label=disable   --security-opt seccomp=unconfined \
          --env="DISPLAY" --env QT_X11_NO_MITSHM=1 \
          --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-         --volume="$DATA_SHARE_DIR:/data:rw" \
+         --volume="$DATA_SHARE_DIR:/home/ros/data:rw" \
          --volume="$GAZEBO_MODEL_DIR:/home/ros/.gazebo/models:rw" \
          --volume="$CONTAINER_INIT_DIR:/home/ros/container-scripts:rw" \
          -p 8888:8888 \
